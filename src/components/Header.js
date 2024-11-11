23cd32723efcb96d63585d4ede35166925e7e829
MarianniArtoni <marianniartoni@edu.unifil.br>
2024-11-11 15:05:19 -0300

    Links para redirecionamento

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { BiLogoGmail } from "react-icons/bi";
-import { IoLogoLinkedin } from "react-icons/io";
+import { IoLogoLinkedin, IoLogoGithub } from "react-icons/io";
 
 const Header = () => {
     return (
-      <header className="text-center">
-        <IoLogoLinkedin />
+      <header className="flex justify-end pr-10 pt-5 gap-2">
         
-        <BiLogoGmail />
+        <a href="https://www.linkedin.com/in/marianniartoni/">
+          <IoLogoLinkedin className="text-3xl" />
+        </a>
+        {/* abrir em outra aba? */}
+        <a href="https://github.com/MarianniArtoni">
+          <IoLogoGithub className="text-3xl" />
+        </a>
+        <a href="mailto:mariartoni12@gmail.com?subject=Contato Profissional" target="_blank" rel="noopener noreferrer">
+          <BiLogoGmail className="text-3xl" />
+        </a>
+        
       </header>
     );
   };

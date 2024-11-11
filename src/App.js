d77ab2bd6fabbce5a217576ad969a0ce503d2384
MarianniArtoni <marianniartoni@edu.unifil.br>
2024-11-11 15:36:42 -0300

    Alteração da cor de fundo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Body from './components/Body.js';
 
 function App() {
   return (
-    <div className='min-h-screen w-full bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-purple-600 to-pink-600'>
+    <div className='min-h-screen w-full bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-[#7f3d9e] to-[#831fe0]'>
       <Header />
       <Body />
     </div>

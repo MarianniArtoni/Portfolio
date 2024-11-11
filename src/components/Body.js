0209c0bab3a89f70793588bd33237957beceb5a0
MarianniArtoni <marianniartoni@edu.unifil.br>
2024-11-11 16:09:09 -0300

    Mudança na fonte

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,7 @@ import React from "react";
 const Body = () => {
     return (
         <section>
-            <h1 className="text-4xl font-bold">Welcome.</h1>
+            <h1 className="text-6xl font-bold flex justify-center mt-10 font-font1">Welcome.</h1>
             <p className="text-xl mt-2">Desenvolvedor Web | Criador de Soluções</p>
         </section>
     );

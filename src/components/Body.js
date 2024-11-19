fc42c94fef404a56b126e9176cd5c77dcca3c2f9
MarianniArtoni <marianniartoni@edu.unifil.br>
2024-11-19 15:13:44 -0300

    Lista dos projetos

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,14 @@ const Body = () => {
     return (
         <section>
             <h1 className="text-6xl font-bold flex justify-center mt-10 font-font1">Welcome.</h1>
-            <p className="text-xl mt-2">Desenvolvedor Web | Criador de Soluções</p>
+            <div className="grid grid-col-12 mt-20">
+                <h2 className="text-4xl font-bold flex col-span-2 col-start-10 font-poiret">Projetos</h2>
+                <ul className="text-xl font-semibold col-span-2 col-start-10 font-poiret list-disc mt-6">
+                    <li>Mulher Plena</li>
+                    <li>UniChoice</li>
+                </ul>
+            </div>
+            
         </section>
     );
 };
